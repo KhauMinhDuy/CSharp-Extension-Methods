82e7f58858e08742fd0bf5be777f947ca8df3d81
khauminhduy <khauminhduy@gmail.com>
2022-08-01 11:07:22 +0700

    update

---
+++ ExtensionMethods.Library/TargerExtentions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods.Library
+{
+    public static class TargerExtentions
+    {
+        internal static void ExtendInternal(this InternalTarget target)
+        {
+
+        }
+    }
+}
---
+++ ExtensionMethods.Library/Target.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods.Library
+{
+    public class Target
+    {
+        private string _id;
+
+        protected string Id
+        {
+            get { return _id; }
+            set { _id = value.Trim(); }
+        }
+
+        public string getId()
+        {
+            return _id;
+        }
+
+        public Target(string id)
+        {
+            _id = id;
+        }
+
+
+    }
+
+    internal class InternalTarget
+    {
+        protected class ProtectedSubclass { }
+    }
+}
