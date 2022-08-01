10901b3c390afe57b66d03191ada877372225b96
khauminhduy <khauminhduy@gmail.com>
2022-08-01 10:32:00 +0700

    initializer

---
+++ ExtensionMethods.Library/ConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace ExtensionMethods.Library
+{
+    public static class ConfigurationExtensions
+    {
+        public static bool IsLoaded(this IConfiguration configuration)
+        {
+            return configuration != null && configuration.AsEnumerable().Any();
+        }
+
+        public static IConfigurationBuilder AddStandardProvider(this IConfigurationBuilder configBuilder)
+        {
+            return configBuilder.AddJsonFile("appsettings.json")
+                                .AddEnvironmentVariables()
+                                .AddJsonFile("config/config.json", optional: true)
+                                .AddJsonFile("secrets/secrets.json", optional: true);
+        }
+    }
+}
+
---
+++ ExtensionMethods.Tests/ConfigurationTests.cs
@@ -0,0 +1,29 @@
+﻿using ExtensionMethods.Library;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods.Tests
+{
+    public class ConfigurationTests
+    {
+        [Test]
+        public void IsLoaded()
+        {
+            IConfiguration configuration = null;
+            Assert.IsFalse(configuration.IsLoaded());
+        }
+
+        [Test]
+        public void AddStandardProviders()
+        {
+            var builder = new ConfigurationBuilder();
+            var config = builder.AddStandardProvider().Build();
+            Assert.AreEqual(4, config.Providers.Count());
+            Assert.IsTrue(config.IsLoaded());
+        }
+    }
+}
