a3e2e8ee9d105e8d7308e83e57baacc3630db7f0
khauminhduy <khauminhduy@gmail.com>
2022-08-10 13:35:04 +0700

    update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExtensionMethods.Contracts;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Starting: {DateTime.Now.Format()}");
+            Console.WriteLine($"Starting: {DateTime.Now.ToUnixSeconds()}");
+            Console.ReadLine();
+        }
+    }
+}
